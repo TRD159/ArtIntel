4174421ba72590c0cf2ecbd4944386ec516af078
TRD159 <rohan360k@gmail.com>
2018-12-18 10:14:47 -0600

    Created a new class for Connect Four frame/panel

---
+++ src/Connect.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Connect {
+
+}
+
+class Frame extends JFrame {
+    Frame(String name) {
+        super(name);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+    }
+}
